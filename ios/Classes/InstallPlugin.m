f646e2aea524ea2c0fab1e666689a71601f19099
hongzhiqing <hzqbetterman@gmail.com>
2020-10-17 23:54:27 +0800

    修改

--- ios/Classes/InstallPlugin.m
+++ ios/Classes/InstallPlugin.m
@@ -1,8 +1,20 @@
 #import "InstallPlugin.h"
-#import <install_plugin/install_plugin-Swift.h>
 
 @implementation InstallPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftInstallPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"install_plugin"
+            binaryMessenger:[registrar messenger]];
+  InstallPlugin* instance = [[InstallPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
